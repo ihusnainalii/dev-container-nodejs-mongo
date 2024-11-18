0b9472b3b95a6b4ad56fd51479f0513c64399b8c
Husnain Ali <imiosengineer@gmail.com>
2024-11-18 16:26:51 +0500

    Updated code
    Updated code

--- docker-entrypoint-initdb.d/mongo-init.js
+++
@@ -1,8 +0,0 @@
-console.log("Started Adding the Users.");
-db = db.getSiblingDB("admin");
-db.createUser({
-  user: "mongo",
-  pwd: "password",
-  roles: [{ role: "root", db: "admin" }],
-});
-console.log("End Adding the User Roles.");
